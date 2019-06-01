94079a242ad6c0199b39cc9e5efc5ce3950fa557
IromanNZ <iroman229man@gmail.com>
2019-06-01 21:44:34 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+$('.header .secondButton').click(
+    function () {
+        $('.header .image').toggle();
+        // console.log('.header .image - toggle');
+        setTimeout(hideButton, 1000)        
+    }
+);
+
+function hideButton() {
+    $('.header .secondButton').insertBefore($('.firstButton'))
+}
+
+$(function(){
+    $('.header .navigation .nav ul li a').on('click', function(e){
+        $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top }, 1000);
+        e.preventDefault();
+      });
+});
+
+$(function(){
+    $('#up-button').on('click', function(e){
+        $('html,body').stop().animate({ scrollTop: $('#home').offset().top }, 1000);
+        e.preventDefault();
+      });
+});
+
+$(function () {
+    $(".portfolio .button").on("click", function(){
+      $(".popup").fadeIn(300);
+    });
+    
+    $(".popup-overlay, .popup-box i").on("click", function(){
+      $(".popup").fadeOut(300);
+    });
+  }); 
